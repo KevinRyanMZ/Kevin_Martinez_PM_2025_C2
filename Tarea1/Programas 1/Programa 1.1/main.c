6ad9869495bd10a492071c8b27b1fbc2aea32719
Darrel <kevinmar282005@gmail.com>
2025-05-28 23:20:35 -0400

    Entrega de tareas

---
+++ Tarea1/Programas 1/Programa 1.1/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/* Programa 1.1
+ El siguiente programa es el primer programa escrito en el lenguaje C para la clase de programación para mecatronicos. */
+
+void main (void)
+{
+    printf("Mi primer programa en C");
+}
